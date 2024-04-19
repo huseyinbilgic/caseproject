c1e4e3b507e91fc502776a1adaf92480608109d2
huseyinbilgic <huseyin.bilgic97@gmail.com>
2024-04-19 09:33:56 +0300

    updated

--- src/main/java/com/caseproject/caseproject/repository/OrderRepository.java
+++ src/main/java/com/caseproject/caseproject/repository/OrderRepository.java
@@ -1,11 +1,14 @@
 package com.caseproject.caseproject.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.caseproject.caseproject.entities.Order;
+import com.caseproject.caseproject.entities.Product;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    List<Order> findByProduct(Product product);
 }
--- src/main/java/com/caseproject/caseproject/service/ProductService.java
+++ src/main/java/com/caseproject/caseproject/service/ProductService.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.caseproject.caseproject.entities.Order;
 import com.caseproject.caseproject.entities.Product;
 import com.caseproject.caseproject.exceptionHandlers.errors.NotFoundException;
 import com.caseproject.caseproject.mapper.ProductMapper;
+import com.caseproject.caseproject.repository.OrderRepository;
 import com.caseproject.caseproject.repository.ProductRepository;
 import com.caseproject.caseproject.requests.ProductRequest;
 import com.caseproject.caseproject.response.ProductResponse;
@@ -20,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
     private final ProductMapper productMapper;
 
     public List<ProductResponse> fetchAllProducts() {
@@ -70,6 +73,10 @@ public class ProductService {
         Optional<Product> productById = productRepository.findById(id);
 
         if (productById.isPresent()) {
+            List<Order> byProduct = orderRepository.findByProduct(productById.get());
+            if (!byProduct.isEmpty()) {
+                throw new NotFoundException("There are orders for this product. This product cannot be deleted.");
+            }
             productRepository.delete(productById.get());
             return "Deleted with ID " + id;
         }
